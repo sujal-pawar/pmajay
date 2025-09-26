c65eec5242aaed85ff24302787307d8edf2664d7
prof7turtle <hairstenyt@gmail.com>
2025-09-26 14:32:46 +0530

    PM ajay Landing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,14 @@
-import pmAjayLogo from './assets/pm-ajay logo.png'
-import azadi75Logo from './assets/azadi75-logo.png'
-import namoImage from './assets/namo.jpeg'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PMajayLanding from './pages/PMajayLanding';
 import './App.css'
 
 function App() {
   return (
-    <div className="app">
-      {/* Navbar */}
-      <nav className="navbar">
-        <div className="navbar-left">
-          <img src={pmAjayLogo} alt="PM-AJAY Logo" className="pm-ajay-logo" />
-          <div className="navbar-text">
-            <h3>Pradhan Mantri Anusuchit Jaati Abhyuday Yojna (PM-AJAY)</h3>
-            <p>Department of Social Justice & Empowerment</p>
-          </div>
-        </div>
-        <div className="navbar-right">
-          <img src={azadi75Logo} alt="Azadi 75 Logo" className="azadi75-logo" />
-        </div>
-      </nav>
-
-      {/* Main Content */}
-      <main className="main-content">
-        <div className="center-image">
-          <img src={namoImage} alt="Namo" className="namo-image" />
-        </div>
-      </main>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<PMajayLanding />} />
+      </Routes>
+    </Router>
   )
 }
 
